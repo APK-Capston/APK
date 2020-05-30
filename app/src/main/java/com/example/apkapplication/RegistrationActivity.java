dab6d943b54a41f4fa6fe60c495c604412e221c1
namyoungjun96 <ehdgornltls@naver.com>
2020-05-30 17:47:57 +0900

    Android Application 1st Commit

---
+++ app/src/main/java/com/example/apkapplication/RegistrationActivity.java
@@ -0,0 +1,28 @@
+package com.example.apkapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        ImageButton back=(ImageButton)findViewById(R.id.back_btn);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplication(), LoginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/apkapplication/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.apkapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd=new Handler();
+        hd.postDelayed(new splashhandler(), 3000);
+    }
+
+    private class splashhandler implements Runnable{
+        public void run(){
+            startActivity(new Intent(getApplication(), LoginActivity.class));
+            SplashActivity.this.finish();
+        }
+    }
+
+    public void onBackPressed(){
+
+    }
+}
